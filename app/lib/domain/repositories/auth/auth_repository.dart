2e8e407329c7a03ef8b0be9b88a1152094a0e89e
KsArT-IT <ksart.it@gmail.com>
2025-07-17 22:07:17 +0300

    feat: update AuthRepository to use the result of the operations

--- app/lib/domain/repositories/auth/auth_repository.dart
+++ app/lib/domain/repositories/auth/auth_repository.dart
@@ -1,27 +1,34 @@
+import 'package:todo_fui/domain/entities/result.dart';
 import 'package:todo_fui/domain/entities/user_entity.dart';
 
 /// {@template auth_repository}
-/// Repository for working with Auth.
+/// Репозиторий для работы с Auth.
 /// {@endtemplate}
 abstract interface class AuthRepository {
   /// Проверить, вошел ли пользователь в систему
   Future<bool> checkIsSignIn();
 
   /// Получить текущего пользователя
-  Future<UserEntity?> getCurrentUser();
+  Future<Result<UserEntity>> getCurrentUser();
 
   /// Войти в систему с помощью email и пароля
-  Future<UserEntity?> signInWithEmailAndPassword(String email, String password);
+  Future<Result<UserEntity>> signInWithEmailAndPassword({
+    required String email,
+    required String password,
+  });
 
   /// Зарегистрироваться с помощью email и пароля
-  Future<UserEntity?> signUpWithEmailAndPassword(String email, String password);
+  Future<Result<UserEntity>> signUpWithEmailAndPassword({
+    required String email,
+    required String password,
+  });
 
   /// Войти в систему с помощью Google
-  Future<UserEntity?> signInWithGoogle();
+  Future<Result<UserEntity>> signInWithGoogle();
 
   /// Выйти из системы
-  Future<void> signOut();
+  Future<Result<void>> signOut();
 
   /// Отправить email для сброса пароля
-  Future<void> sendPasswordResetEmail(String email);
+  Future<Result<void>> sendPasswordResetEmail(String email);
 }
