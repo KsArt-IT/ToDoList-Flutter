93c1e1db53937393d62e44aad340e785a597d100
KsArT-IT <ksart.it@gmail.com>
2025-07-19 22:20:26 +0300

    feat: update password validation regex and add validation methods

--- app/lib/domain/entities/password.dart
+++ app/lib/domain/entities/password.dart
@@ -2,9 +2,11 @@ final class Password {
   final String value;
   const Password({required this.value});
 
-  static final _passwordRegex = RegExp(
-    r'^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,64}$',
-  );
-
   bool get isValid => _passwordRegex.hasMatch(value);
+
+  static bool validate(String value) => _passwordRegex.hasMatch(value);
+
+  static final _passwordRegex = RegExp(
+    r'^(?=.*[A-Za-z])(?=.*\d)[A-Za-z0-9_+\-!@.$%^&*()]{8,64}$'
+  );
 }
