c45997b349b5a1149d4081eb2b31947f772e34bd
KsArT-IT <ksart.it@gmail.com>
2025-07-19 22:19:43 +0300

    feat: update email validation regex and add validation methods

--- app/lib/domain/entities/email.dart
+++ app/lib/domain/entities/email.dart
@@ -3,9 +3,12 @@ final class Email {
 
   const Email({required this.value});
 
-  static final _emailRegex = RegExp(
-    r'^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$',
-  );
-
   bool get isValid => _emailRegex.hasMatch(value);
+
+  static bool validate(String value) => _emailRegex.hasMatch(value);
+
+  static final _emailRegex = RegExp(
+    r'^[0-9a-zA-Z]+([0-9a-zA-Z]*[-._+])*[0-9a-zA-Z]+@[0-9a-zA-Z]+'
+    r'([-.][0-9a-zA-Z]+)*([0-9a-zA-Z]*[.])[a-zA-Z]{2,6}$',
+  );
 }
