1616247d995306a64a735bcae0edbcdd7744b727
KsArT-IT <ksart.it@gmail.com>
2025-07-24 21:00:00 +0300

    feat: update RegistrationBloc to support user signout

--- app/lib/presentation/pages/registration/bloc/registration_bloc.dart
+++ app/lib/presentation/pages/registration/bloc/registration_bloc.dart
@@ -3,17 +3,21 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:todo_fui/domain/entities/email.dart';
 import 'package:todo_fui/domain/entities/password.dart';
 import 'package:todo_fui/domain/entities/state_status.dart';
-import 'package:todo_fui/domain/usecases/auth/sign_up_with_email_and_password_use_case.dart';
+import 'package:todo_fui/domain/usecases/auth_use_cases.dart';
 
 part 'registration_event.dart';
 part 'registration_state.dart';
 
 class RegistrationBloc extends Bloc<RegistrationEvent, RegistrationState> {
   final SignUpWithEmailAndPasswordUseCase _signUpWithEmailAndPasswordUseCase;
+  final SignOutUseCase _signOutUseCase;
 
-  RegistrationBloc({required SignUpWithEmailAndPasswordUseCase signUpWithEmailAndPasswordUseCase})
-    : _signUpWithEmailAndPasswordUseCase = signUpWithEmailAndPasswordUseCase,
-      super(RegistrationState()) {
+  RegistrationBloc({
+    required SignUpWithEmailAndPasswordUseCase signUpWithEmailAndPasswordUseCase,
+    required SignOutUseCase signOutUseCase,
+  }) : _signUpWithEmailAndPasswordUseCase = signUpWithEmailAndPasswordUseCase,
+       _signOutUseCase = signOutUseCase,
+       super(RegistrationState()) {
     on<EmailChanged>(_onEmailChanged);
     on<PasswordChanged>(_onPasswordChanged);
     on<PasswordConfirmChanged>(_onPasswordConfirmChanged);
@@ -22,32 +26,17 @@ class RegistrationBloc extends Bloc<RegistrationEvent, RegistrationState> {
 
   void _onEmailChanged(EmailChanged event, Emitter<RegistrationState> emit) {
     final email = Email(value: event.email);
-    emit(
-      state.copyWith(
-        email: email,
-        status: StateStatus.initial,
-      ),
-    );
+    emit(state.copyWith(email: email, status: StateStatus.initial));
   }
 
   void _onPasswordChanged(PasswordChanged event, Emitter<RegistrationState> emit) {
     final password = Password(value: event.password);
-    emit(
-      state.copyWith(
-        password: password,
-        status: StateStatus.initial,
-      ),
-    );
+    emit(state.copyWith(password: password, status: StateStatus.initial));
   }
 
   void _onPasswordConfirmChanged(PasswordConfirmChanged event, Emitter<RegistrationState> emit) {
     final passwordConfirm = Password(value: event.password);
-    emit(
-      state.copyWith(
-        passwordConfirm: passwordConfirm,
-        status: StateStatus.initial,
-      ),
-    );
+    emit(state.copyWith(passwordConfirm: passwordConfirm, status: StateStatus.initial));
   }
 
   Future<void> _onRegistrationRequest(
@@ -61,14 +50,12 @@ class RegistrationBloc extends Bloc<RegistrationEvent, RegistrationState> {
       password: state.password.value,
     );
     result.map(
-      onSuccess: (_) => emit(state.copyWith(status: StateStatus.success)),
+      onSuccess: (_) async {
+        await _signOutUseCase();
+        emit(state.copyWith(status: StateStatus.success));
+      },
       onFailure: (error) {
-        emit(
-          state.copyWith(
-            errorMessage: error.toString(),
-            status: StateStatus.error,
-          ),
-        );
+        emit(state.copyWith(errorMessage: error.toString(), status: StateStatus.error));
       },
     );
   }
