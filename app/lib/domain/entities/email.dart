ec1d6c3320776dc025c1febea79a2976a05111d1
KsArT-IT <ksart.it@gmail.com>
2025-07-31 23:46:16 +0300

    fix: update regular expression for email address validation

--- app/lib/domain/entities/email.dart
+++ app/lib/domain/entities/email.dart
@@ -7,8 +7,5 @@ final class Email {
 
   static bool validate(String value) => _emailRegex.hasMatch(value);
 
-  static final _emailRegex = RegExp(
-    r'^[0-9a-zA-Z]+([0-9a-zA-Z]*[-._+])*[0-9a-zA-Z]+@[0-9a-zA-Z]+'
-    r'([-.][0-9a-zA-Z]+)*([0-9a-zA-Z]*[.])[a-zA-Z]{2,6}$',
-  );
+  static final _emailRegex = RegExp(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$');
 }
