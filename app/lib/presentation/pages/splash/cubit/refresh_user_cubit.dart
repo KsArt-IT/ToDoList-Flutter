7ab5c4b15285393cd47bc7eac43327bd6cbb5ce3
KsArT-IT <ksart.it@gmail.com>
2025-08-23 17:25:42 +0300

    feat: add qubit to update user with state handling logic

---
+++ app/lib/presentation/pages/splash/cubit/refresh_user_cubit.dart
@@ -0,0 +1,28 @@
+import 'dart:developer';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo_fui/domain/entities/user_entity.dart';
+import 'package:todo_fui/domain/usecases/auth/refresh_user_use_case.dart';
+
+part 'refresh_user_state.dart';
+
+class RefreshUserCubit extends Cubit<RefreshUserState> {
+  final RefreshUserUseCase _refreshUserUseCase;
+
+  RefreshUserCubit({required RefreshUserUseCase refreshUserUseCase})
+    : _refreshUserUseCase = refreshUserUseCase,
+      super(RefreshUserInitial()) {
+    _refreshUser();
+  }
+
+  Future<void> _refreshUser() async {
+    log('_refreshUser start', name: 'RefreshUserCubit');
+    await Future.delayed(const Duration(seconds: 2));
+    final result = await _refreshUserUseCase();
+    result.map(
+      onSuccess: (user) => emit(RefreshUserSuccess(user)),
+      onFailure: (error) => emit(RefreshUserFailure()),
+    );
+  }
+}
