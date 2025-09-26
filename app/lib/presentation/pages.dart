1b5d71bbe9b7e8391fa2622d0a0b2e124adabd16
KsArT-IT <ksart.it@gmail.com>
2025-09-26 19:15:08 +0300

    feat: create pages.dart to export various presentation pages

---
+++ app/lib/presentation/pages.dart
@@ -0,0 +1,8 @@
+export 'package:todo_fui/presentation/pages/home/home_page.dart';
+export 'package:todo_fui/presentation/pages/splash/splash_page.dart';
+export 'package:todo_fui/presentation/pages/login/login_page.dart';
+export 'package:todo_fui/presentation/pages/registration/registration_page.dart';
+export 'package:todo_fui/presentation/pages/reset_password/reset_password_page.dart';
+export 'package:todo_fui/presentation/pages/edit/edit_page.dart';
+export 'package:todo_fui/presentation/pages/profile/profile_page.dart';
+export 'package:todo_fui/presentation/pages/settings/settings_page.dart';
